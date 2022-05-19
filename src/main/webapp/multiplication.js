4133c027152c37cb228b950788b3d29688307197
venu <venugopalsvg10@gmail.com>
2022-05-19 10:00:33 +0530

    done

--- src/main/webapp/multiplication.js
+++ src/main/webapp/multiplication.js
@@ -3,6 +3,6 @@ var a = document.getElementById("firstno").value
 var b = document.getElementById("secondno").value
 var result
 result = a*b
-document.getElementById("result").value=result
+document.getElementById("passs").value=result
 }
 
